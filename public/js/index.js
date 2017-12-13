aa027d45a9f7206615eaa70e0d38f3951aa828f2
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2017-12-13 10:00:09 +0200

    feat - added emit and listener events on both the server and the browser

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    var body = {
+        from: 'jen@example.com',
+        text: 'I prefer burgers'
+    };
+
+    // emitter
+    socket.emit('create_message', body);
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnecting from server');
+});
+
+// listener
+socket.on('new_message', function(message) {
+    console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,19 +1,50 @@
 const path = require('path');
+const http = require('http');
 
 const express = require('express');
+const socketIO = require('socket.io');
+
+const server_settings_service = require('./server.settings');
 
 const public_path = path.join(__dirname, '../public');
-
 const PORT = process.env.PORT || 3000;
 
 let app = express();
+let server = http.createServer(app); // or let server = http.createServer((req, res) => {});
+let io = socketIO(server);
 
 app.use(express.static(public_path));
+app.use(server_settings_service.log_url);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    let body = {
+        from: 'mike@emaple.com',
+        text: 'Hey, I like pie',
+        created_at: new Date().toISOString()
+    };
+    
+    // emitter
+    socket.emit('new_message', body);
+
+    // listener
+    socket.on('create_message', (message) => {
+        console.log('Create message', message);
+    });
+
+    // listener
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('App up and running');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Express app started successfully on port ${PORT}`);
 });
